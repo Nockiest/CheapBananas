49f0c31e4910e704b8e287b5a8a9431686066e9f
Nockiest <ondralukes06@seznam.cz>
2025-05-18 10:09:25 +0200

    reworked database values

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,43 @@ use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
 use std::env;
 use bigdecimal::BigDecimal;
+use uuid::Uuid;
 #[derive(Debug, FromRow, Serialize, Deserialize)]
 struct Product {
-    id: i32,
+    id: Uuid,
     name: String,
     price: f64,
     product_volume: Option<f64>,
-    shop_id: Option<i32>,
+    shop_id: Option<Uuid>,
     date: Option<NaiveDateTime>,
     notes: Option<String>,
     tags: Option<Vec<String>>,
 }
 
 struct Shop {
-    id: i32,
+    id: Uuid,
     name: String,
     products: Vec<Product>,
 }
 async fn get_products(pool: &PgPool) -> Result<Vec<Product>, sqlx::Error> {
     let products: Vec<Product> = sqlx::query_as::<_, Product>(
-        "SELECT id, name, price, product_volume, shop_id, date, notes, tags FROM product",
+        "SELECT id, name, price, product_volume, shop_id, date, notes, tags FROM products",
     )
     .fetch_all(pool)
     .await?;
     Ok(products)
 }
 
-async fn add_shop(pool: &PgPool, name: &str) -> Result<i32, sqlx::Error> {
-    let shop = sqlx::query!("INSERT INTO shop (name) VALUES ($1) RETURNING id", name)
+async fn add_shop(pool: &PgPool, name: &str) -> Result<Uuid, sqlx::Error> {
+    let shop = sqlx::query!("INSERT INTO shops (name) VALUES ($1) RETURNING id", name)
         .fetch_one(pool)
         .await?;
     Ok(shop.id)
 }
 
-async fn add_product(pool: &PgPool, product: &Product) -> Result<i32, sqlx::Error> {
+async fn add_product(pool: &PgPool, product: &Product) -> Result<Uuid, sqlx::Error> {
     let product = sqlx::query!(
-        "INSERT INTO product (name, price, product_volume, shop_id, date, notes, tags) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+        "INSERT INTO products (name, price, product_volume, shop_id, date, notes, tags) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
         product.name,
         product.price,
         product.product_volume,
@@ -57,17 +58,16 @@ async fn main() -> Result<(), sqlx::Error> {
     dotenv::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPool::connect(&database_url).await?;
-    add_shop(&pool, "Test Shop").await?;
-
+    let shop_id: Uuid = add_shop(&pool, "Test Shop").await?;
     let test_product: Product = Product {
-        id: 1,
+        id: Uuid::new_v4(),
         name: "Test Product".to_string(),
         price: 10.0,
         product_volume: Some(1.0),
-        shop_id: Some(1),
+        shop_id: Some(shop_id),
         date: None, // Uncomment and set if needed
         notes: Some("Test notes".to_string()),
-        tags: None//Some(vec!["tag1".to_string(), "tag2".to_string()]),
+        tags: Some(vec!["tag1".to_string(), "tag2".to_string()]),
     };
 
     add_product(&pool, &test_product).await?;
